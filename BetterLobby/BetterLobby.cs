8f27199fc389314996c78220118e6899f2907126
nyawoi <106277673+nyawoi@users.noreply.github.com>
2023-12-23 08:35:02 -0600

    Renamed base project

--- BetterLobby/BetterLobby.cs
+++ BetterLobby/BetterLobby.cs
@@ -1,13 +1,13 @@
 ﻿using BepInEx;
 
-namespace ExamplePlugin;
+namespace AetharNet.Mods.ZumbiBlocks2.BetterLobby;
 
 [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-public class ExamplePlugin : BaseUnityPlugin
+public class BetterLobby : BaseUnityPlugin
 {
-    public const string PluginGUID = PluginAuthor + "." + PluginName;
-    public const string PluginAuthor = "PluginAuthor";
-    public const string PluginName = "PluginName";
+    public const string PluginGUID = "AetharNet.Mods.ZumbiBlocks2.BetterLobby";
+    public const string PluginAuthor = "awoi";
+    public const string PluginName = "BetterLobby";
     public const string PluginVersion = "0.1.0";
     
     private void Awake()
