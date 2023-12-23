554a0a7cc0fae2aef50eff4ced101c1e2d18f734
nyawoi <106277673+nyawoi@users.noreply.github.com>
2023-12-23 14:52:29 -0600

    Implement visual feedback for ready state

--- BetterLobby/BetterLobby.cs
+++ BetterLobby/BetterLobby.cs
@@ -1,7 +1,7 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AetharNet.Mods.ZumbiBlocks2.BetterLobby;
 
@@ -12,14 +12,16 @@ public class BetterLobby : BaseUnityPlugin
     public const string PluginAuthor = "awoi";
     public const string PluginName = "BetterLobby";
     public const string PluginVersion = "0.1.0";
-
-    private new static ManualLogSource Logger;
     
+    private static Sprite YesIcon;
+    private static Sprite NoIcon;
+
     private void Awake()
     {
-        Logger = base.Logger;
         On.LobbyMenu.OnEnable += On_LobbyMenu_OnEnable;
         On.LobbyMenu.LaunchMatch += On_LobbyMenu_LaunchMatch;
+        On.LobbyController.AddPlayer += On_LobbyController_AddPlayer;
+        On.LobbyController.SetPlayerReady += On_LobbyController_SetPlayerReady;
     }
 
     private static void On_LobbyMenu_OnEnable(On.LobbyMenu.orig_OnEnable orig, LobbyMenu self)
@@ -29,7 +31,7 @@ public class BetterLobby : BaseUnityPlugin
         {
             tag = "<insane>",
             imgSprite = null,
-            textColor = new Color32(208, 89,232, 255)
+            textColor = new Color32(208, 89, 232, 255)
         });
 
         // If not the lobby host, remove difficulty selector buttons
@@ -38,8 +40,27 @@ public class BetterLobby : BaseUnityPlugin
         {
             self.difficultySelectorButtons.Do(Destroy);
         }
+
+        // Retrieve icons for visual feedback
+        foreach (var sprite in Resources.FindObjectsOfTypeAll<Sprite>())
+        {
+            if (sprite is not null)
+            {
+                switch (sprite.name)
+                {
+                    case "YesIcon":
+                        YesIcon ??= sprite;
+                        break;
+                    case "NoIcon":
+                        NoIcon ??= sprite;
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
     }
-    
+
     private static void On_LobbyMenu_LaunchMatch(On.LobbyMenu.orig_LaunchMatch orig, LobbyMenu self)
     {
         // Only start match if all players are ready
@@ -47,5 +68,30 @@ public class BetterLobby : BaseUnityPlugin
         {
             orig(self);
         }
+    }
+    
+    private static int On_LobbyController_AddPlayer(On.LobbyController.orig_AddPlayer orig, LobbyController self, SteamP2PConnection connection, string playername, SkinDatabase.SkinID skinid, SkinDatabase.Gender skingender, int skincolor, bool applyloadout)
+    {
+        // If the connection is null, the host is starting a server;
+        // otherwise, it's a player joining
+        // Set the ready icon to false
+        if (connection != null)
+        {
+            var startButtonObject = self.lobbyMenu.startButton.transform.GetChild(1).gameObject;
+            var buttonImage = startButtonObject.GetComponent<Image>();
+            buttonImage.overrideSprite = NoIcon;
+        }
+        
+        return orig(self, connection, playername, skinid, skingender, skincolor, applyloadout);
+    }
+
+    private static void On_LobbyController_SetPlayerReady(On.LobbyController.orig_SetPlayerReady orig, LobbyController self, int lobbyid, bool isready)
+    {
+        // Call the original method first, so the `isReady` field is set
+        orig(self, lobbyid, isready);
+        
+        var startButtonObject = self.lobbyMenu.startButton.transform.GetChild(1).gameObject;
+        var buttonImage = startButtonObject.GetComponent<Image>();
+        buttonImage.overrideSprite = self.players.TrueForAll(player => player.isReady) ? null : NoIcon;
     }
 }
